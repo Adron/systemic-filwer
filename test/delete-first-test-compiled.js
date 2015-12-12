5b9c31ffa51760dc3b2d40eb8a76c443ebbd2e68
Adron Hall <adron_hall@homedepot.com>
2015-12-11 17:03:48 -0800

    Build fix.

---
+++ test/delete-first-test-compiled.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Created by Adron on 12/11/15.
+ * Description: Template for node.js projects.
+ */
+
+var assert = require('assert');
+describe('Because', function () {
+  describe('true', function () {
+    it('should be true', function () {
+      assert.equal(true, true);
+    });
+  });
+});
+
+//# sourceMappingURL=delete-first-test-compiled.js.map
---
+++ test/delete-first-test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Adron on 12/11/15.
+ * Description: Template for node.js projects.
+ */
+
+var assert = require('assert');
+describe('Because', function() {
+  describe('true', function () {
+    it('should be true', function () {
+      assert.equal(true, true);
+    });
+  });
+});
